28c5ba22619d74370b0c342d5fdf46a0d5ee8a42
Elena Bogdanovich <elen.vaylen@gmail.com>
2020-09-13 16:21:14 +0300

    add updated build

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,16 +3,19 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import { Container } from '@material-ui/core'
 import Styled from './styles'
 
 const Header: React.FC = () => (
   <Styled className='header'>
     <AppBar position='static'>
-      <Toolbar>
-        <a href='/' className='header__logo'>
-          <Typography className='header__logo_text'>Pokemons</Typography>
-        </a>
-      </Toolbar>
+      <Container>
+        <Toolbar disableGutters>
+          <a href='/' className='header__logo'>
+            <Typography className='header__logo_text'>Pokemons</Typography>
+          </a>
+        </Toolbar>
+      </Container>
     </AppBar>
   </Styled>
 )
